98ebc5062dff0f9256258767583a365498b6e460
Abhishek Gupta <guptaa476.ag@gmail.com>
2024-06-27 15:17:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Utils/Themes";
+import Navbar from "./components/Navbar";
+import Hero from "./components/HeroSection";
+import Skills from "./components/Skills";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+          </Wrapper>
+        </Body>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      HeroSection
+    </div>
+  )
+}
+
+export default HeroSection
